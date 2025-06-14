696ccfb136907742418dd51559bef8447f3573fa
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2025-06-14 16:20:03 +0530

    fix disallow rules in robots

--- web/app/robots.ts
+++ web/app/robots.ts
@@ -3,21 +3,6 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/admin/', '/_next/'],
-      },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      },
     ],
     sitemap: 'https://www.al-ifta.com/sitemap.xml',
     host: 'https://www.al-ifta.com',
